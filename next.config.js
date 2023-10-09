1cdb2b506e46c669339fc4e6a3b53e8a39c5e6aa
Piotr Pietryka <eveo.piotr@gmail.com>
2023-10-09 23:26:27 +0200

    [Add] Vercel new config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: `/api/:path*`,
-            destination: `/api/:path*`,
-          },
-          {
-            source: `/:path*`,
-            destination: `${process.env.NEXT_PUBLIC_URL}/:path*`,
-          },
-        ];
-      },
+
 }
 
 module.exports = nextConfig

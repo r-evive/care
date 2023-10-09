5c716a51fde6e4785ca8bd07ae62499a7777fc2a
Piotr Pietryka <eveo.piotr@gmail.com>
2023-10-09 23:14:50 +0200

    [Add] Next config redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: `/api/:path*`,
+            destination: `/api/:path*`,
+          },
+          {
+            source: `/:path*`,
+            destination: `${process.env.NEXT_PUBLIC_URL}/:path*`,
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig

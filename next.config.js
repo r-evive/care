af15cb05309f41243ffd38fdbbb30c59d362bd4b
Piotr Pietryka <eveo.piotr@gmail.com>
2023-10-09 22:22:55 +0200

    [Remove] Remove redirects

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/:path*',
-            destination: `${process.env.NEXT_PUBLIC_URL}/:path*`,
-            permanent: true,
-          },
-        ];
-      },
 }
 
 module.exports = nextConfig

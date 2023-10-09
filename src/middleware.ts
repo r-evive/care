adc629c75ad0c37ce5cdfd394153640df90fe4d5
Piotr Pietryka <eveo.piotr@gmail.com>
2023-10-09 21:59:34 +0200

    [Add] Callback error trying to fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,7 @@ const anonymousRoutes = ['/login', '/register', '/api/user/register']; // The wh
 export default withAuth(
     async function middleware(request:any, response:any,) {
         const { pathname } = request.nextUrl;
+        console.log('From middleware', pathname);
         const accessToken = request.headers.get('Authorization')?.replace('Bearer ', '');
         console.log('From middleware', accessToken);
 
@@ -34,7 +35,15 @@ export default withAuth(
     callbacks: {
         authorized: ({ req }) => {
             const { pathname } = req.nextUrl;
-  
+            console.log("Callback", pathname, Boolean(
+                req.cookies.get('next-auth.session-token') || // check if there's a token
+                    pathname.startsWith('/_next') || // exclude Next.js internals
+                    pathname.startsWith('/static') || // exclude static files
+                    pathname.startsWith('/api') || // exclude API routes
+                    publicFileRegex.test(pathname) || // exclude all files in the public folder
+                    anonymousRoutes.includes(pathname)
+            ));
+            
             return Boolean(
                 req.cookies.get('next-auth.session-token') || // check if there's a token
                     pathname.startsWith('/_next') || // exclude Next.js internals

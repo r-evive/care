bf76e1e38b8aba9739e4fb99f553235fcb5ba290
Piotr Pietryka <eveo.piotr@gmail.com>
2023-10-09 22:20:51 +0200

    [Add] Next config redirects matcher

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/:path*',
+            destination: `${process.env.NEXT_PUBLIC_URL}/:path*`,
+            permanent: true,
+          },
+        ];
+      },
+}
 
 module.exports = nextConfig

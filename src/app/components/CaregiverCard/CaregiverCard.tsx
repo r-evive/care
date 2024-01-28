fa70ac831ce0cfd3a64d29766da6e16259627307
Piotr Pietryka <eveo.piotr@gmail.com>
2024-01-28 09:21:14 +0100

    [Fix] Build error

--- src/app/components/CaregiverCard/CaregiverCard.tsx
+++ src/app/components/CaregiverCard/CaregiverCard.tsx
@@ -9,7 +9,7 @@ const CaregiverCard = async (props: Props) => {
 
     return (
         <div className="flex flex-col items-center ">
-            <div href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-6xl hover:bg-gray-100 ">
+            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-6xl hover:bg-gray-100 ">
                 <div className="md:w-96 w-full h-full border-solid border-r-2 border-gray-700">
                     TEST
                 </div>

413e288ea1c034cfb94217e1350552d99c129fa6
Piotr Pietryka <eveo.piotr@gmail.com>
2024-04-24 23:29:25 +0200

    [Fix] Links in profile

--- src/layouts/ProfileLayout/ProfileMenuItem/ProfileMenuItem.tsx
+++ src/layouts/ProfileLayout/ProfileMenuItem/ProfileMenuItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { TProfileMenu } from "@/models/ProfileMenu"
 import { useRouter } from "next/navigation"
+import Link from 'next/link'
 
 
 const ProfileMenuItem = ({item, index, path} : {item:TProfileMenu, index: number, path: string}) => {
@@ -11,15 +12,13 @@ const ProfileMenuItem = ({item, index, path} : {item:TProfileMenu, index: number
         return pathname === path ? 'bg-blue-500 text-white hover:bg-blue-500': '';
     }
 
-    const handleOnClick = () => {
-        router.push(`/profile/${item.path}`);
-    }
-
     return (
-        <div className={`flex items-center gap-2 py-2 px-2 rounded cursor-pointer transition text-gray-700 hover:bg-blue-100 ${isMenuItemSelected(item.path)}`} key={index} onClick={handleOnClick}>
-            <span className="text-lg">{item.icon}</span>
-            <span className="text-lg">{item.label}</span>
-        </div>
+        <Link href={`/profile/${item.path}`}>
+            <div className={`flex items-center gap-2 py-2 px-2 rounded cursor-pointer transition text-gray-700 hover:bg-blue-100 ${isMenuItemSelected(item.path)}`} key={index}>
+                <span className="text-lg">{item.icon}</span>
+                <span className="text-lg">{item.label}</span>
+            </div>
+        </Link>
     )
 }
 
